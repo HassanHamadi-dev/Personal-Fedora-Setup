5e5bde16a33263e786d231b7ce2faa0b6f2739ac
Hassan Hamadi <103293050+sheriffwastaken@users.noreply.github.com>
2023-11-20 00:36:26 -0500

    Update gnome-setup.sh

--- gnome-setup.sh
+++ gnome-setup.sh
@@ -12,6 +12,25 @@ cd "$script_dir" || exit 1
 
 MY_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 
+
+move_folder() {
+    source_folder_path="$1"
+    target_folder_path="$2"
+
+    if [ -d "$source_folder_path" ]; then
+        echo "Moving $source_folder_path to $target_folder_path"
+        mv "$source_folder_path" "$target_folder_path"
+        notify-send "$source_folder_path Moved" "$source_folder_path moved to $target_folder_path" --expire-time=10 --icon=dialog-information --urgency=low --category=system
+    else
+        notify-send "Error" "$source_folder_path not found in the repository." --expire-time=10 --icon=dialog-information --urgency=low --category=system
+    fi
+}
+
+move_folder "./.local/share/fonts" "$HOME/.local/share/"
+move_folder "./walls" "$HOME/Pictures/"
+move_folder "./alacritty" "$HOME/.config/"
+move_folder "./.zshrc" "$HOME/"
+
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=6
@@ -35,7 +54,6 @@ while true; do
                    7 "Install Nvidia Drivers Akmod-Nvidia" \
                    8 "exit" \
                    3>&1 1>&2 2>&3)
-
 
     case $choice in
         1)
