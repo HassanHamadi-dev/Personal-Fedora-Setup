b5d37d04224cfef6ecbfba2316666288008bf9b8
Hassan Hamadi <hassan@hamadi.dev>
2023-12-02 01:06:50 -0500

    Update kde-setup.sh
    changed starship to p10k

--- kde-setup.sh
+++ kde-setup.sh
@@ -47,7 +47,7 @@ while true; do
             sudo dnf clean all
             sudo dnf install dnf-plugins-core
             sudo dnf config-manager --setopt=fastestmirror=true --save
-            notify-send "DNF has been sped up" --expire-time=10 --icon=dialog-information --urgency=low --category=system
+            notify-send "DNF has been sped up" --expire-time=10  
             ;;
             
         2)
@@ -57,7 +57,7 @@ while true; do
             sudo dnf groupupdate -y core
             sudo dnf install -y rpmfusion-free-release-tainted
             sudo dnf install -y dnf-plugins-core
-            notify-send "RPM Fusion Enabled" --expire-time=10 --icon=dialog-information --urgency=low --category=system
+            notify-send "RPM Fusion Enabled" --expire-time=10  
             ;;
 
         3)
@@ -95,21 +95,20 @@ while true; do
 
                 echo "Installation completed."
                 source dnf-package-extra-setup.sh
-                notify-send "Software has been installed with DNF" --expire-time=10 --icon=dialog-information --urgency=low --category=system
+                notify-send "Software has been installed with DNF" --expire-time=10
             else
                 echo "Error: Package file $package_file not found."
-                notify-send "Error" "Package file $package_file not found." --expire-time=10 --icon=dialog-error --urgency=low --category=system
+                notify-send "Error" "Package file $package_file not found." --expire-time=10
             fi
             ;;
             
         5)
-            echo "Setting Up Oh-My-Zsh & Starship..."
+            echo "Setting Up Oh-My-Zsh & powerlevel10k..."
             sudo dnf -y install zsh util-linux-user
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
             chsh -s "$(which zsh)"
-            curl -sS https://starship.rs/install.sh | sh
-            echo "eval "$(starship init zsh)"" >> ~/.zshrc
-            notify-send "Oh-My-Zsh & Starship are ready to go" --expire-time=10 --icon=dialog-information --urgency=low --category=system
+            git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+            notify-send "Oh-My-Zsh & powerlevel10k are ready to go" --expire-time=10 
             ;;
 
         6)
@@ -142,7 +141,7 @@ while true; do
             cd Fluent-icon-theme/
             ./install.sh -a
 
-            notify-send "Themes/Fonts/Codecs Complete" --expire-time=10 --icon=dialog-information --urgency=low --category=system   
+            notify-send "Themes/Fonts/Codecs Complete" --expire-time=10  
             ;;
 
         7)
@@ -150,9 +149,9 @@ while true; do
             sudo dnf install -y akmod-nvidia
             # Check if the NVIDIA kernel module is loaded
             if lsmod | grep -q "^nvidia "; then
-                notify-send "Kernel Module Loaded" "The NVIDIA kernel module is loaded. It's safe to reboot." --expire-time=10 --icon=dialog-information --urgency=low --category=system
+                notify-send "Kernel Module Loaded" "The NVIDIA kernel module is loaded. It's safe to reboot." --expire-time=10  
             else
-                notify-send "Kernel Module Not Loaded" "The NVIDIA kernel module is not loaded yet." --expire-time=10 --icon=dialog-information --urgency=low --category=system
+                notify-send "Kernel Module Not Loaded" "The NVIDIA kernel module is not loaded yet." --expire-time=10  
             fi
             ;;
 
